48e03a203dcc6adb1a1f84fbd31731bb8d2011fd
Enzo Yuta Nagasava <enzonagasava@gmail.com>
2025-11-06 16:03:46 -0300

    FM-26

---
+++ lang/pt_BR/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de linguagem de paginação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são usadas pela biblioteca do paginador
+    | para construir os links de paginação simples. Você pode alterá-las para
+    | qualquer coisa que desejar para personalizar suas visualizações e deixar
+    | sua aplicação mais consistente.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
